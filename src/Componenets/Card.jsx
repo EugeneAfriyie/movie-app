1c5c4925fe35e3810a855da9caf412eb85c064b0
Eugene Afriyie <groupeight00@gmail.com>
2025-03-08 20:12:10 +0000

    fetching movie data from rapid api

--- src/Componenets/Card.jsx
+++ src/Componenets/Card.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({cardWidth}) => {
   return (
-    <div>
-        <div>
-            <h1>The Godfather</h1>
-            <div>
-                <span>Genres</span>
-                <span>Crime Drama</span>
+    <div style={{width:cardWidth}} className='h-[650px] relative flex justify-center items-center shrink-0 p-2 bg-gray-600 group '>
+        <div className='w-[97%] h-[97%] m-auto text-white absolute rounded-lg bg-black/50 flex flex-col justify-center gap-y-2 p-10 cursor-pointer '>
+            <h1 className=' text-4xl '>The Godfather</h1>
+            <div className='flex gap-x-2 items-center'>
+                <span className='text-lg '>Genres</span>
+                <span className='font-semibold text-red-600'>Crime Drama</span>
             </div>
-            <span>Original Language: <span>En</span></span>
-            <span>Released Date:<span>1792-03-36</span></span>
-            <p>
-                <span>Summary</span>
-                <span>Summary text</span>
+            <span className='flex gap-x-2 '>Original Language: <span className='mr-2 uppercase'>En</span></span>
+            <span className='flex gap-x-2' >Released Date:<span className='mr-2 text-yellow-400' >1792-03-36</span></span>
+            <p className='flex flex-col gap-y-1 '>
+                <span className='text-red-500  '>Summary</span>
+                <span className='first-letter:pl-2 '>Summary text</span>
             </p>
         </div>
 
-        <img src="/download.jpg" alt="Movie image" />
+        <img src="/download.jpg"
+             alt="Movie image"
+             className='object-cover rounded-xl opacity-50 group-hover:opacity-100 transition-opacity duration-500 absolute w-[97%] h-[97%] '
+             />
     </div>
   )
 }
--- src/Componenets/Home.jsx
+++ src/Componenets/Home.jsx
@@ -1,11 +1,48 @@
+import { useEffect, useState } from "react"
 import Card from "./Card"
 
 const Home = () => {
+    const [cardWidth,setCardWidth] = useState(500)
+    const cardsInRow = 5;
+    const [wrapperWidth,setWrapperWidth] = useState(cardWidth * cardsInRow);
+
+    const  [movie,setMovie] = useState([]);
+
+
+    useEffect(() => {
+        const getMovie = async () => {
+            const url = 'https://tvshow.p.rapidapi.com/Movie/NowPlaying?Page=1&Language=en-US&Adult=true';
+            const options = {
+                method: 'GET',
+                headers: {
+                    'x-rapidapi-key': 'bbb670fbe7msh06e0ff277b81e75p1203c3jsnbdb467292e56',
+                    'x-rapidapi-host': 'tvshow.p.rapidapi.com'
+                }
+            };
+    
+            try {
+                const response = await fetch(url, options);
+                const result = await response.text();
+                console.log(result);
+                setMovie(result)
+            } catch (error) {
+                console.error(error);
+                alert(error)
+            }
+        };
+    
+        getMovie(); // ✅ Call the function inside useEffect
+    }, []);
+    
+
   return (
-    <div>
-        <div>
+        <div className='flex justify-center items-center' style={{width: wrapperWidth}} >
+        <div className="flex flex-wrap ">
+            {movie.map(({movie,i}) => {
+                
+            })}
             <div>
-                <Card />
+                <Card cardWidth={cardWidth}/>
             </div>
         </div>
     </div>
