95beda360e4756f25b536e6cecd22cb6f33b1948
Eugene Afriyie <groupeight00@gmail.com>
2025-03-08 18:24:43 +0000

    created components for the projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import './App.css'
+import Home from "./Componenets/Home"
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Home />
+    </div>
   )
 }
 
