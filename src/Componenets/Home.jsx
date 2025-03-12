62d23d4b25a796d3d8783b2deb80678e832db6dc
Eugene Afriyie <groupeight00@gmail.com>
2025-03-12 01:12:26 +0000

    on the pagination

--- src/Componenets/Home.jsx
+++ src/Componenets/Home.jsx
@@ -8,14 +8,19 @@ const Home = () => {
     const cardsInRow = 5;
     const [wrapperWidth, setWrapperWidth] = useState(cardWidth * cardsInRow);
     const [movies, setMovies] = useState([]);
+    const [page,setPage] = useState(1)
+    const [group,setgroup] = useState('Popular') 
+
+    const apiKey = import.meta.env.VITE_API_KEY 
+    const baseUrl = import.meta.env.VITE_BASE_URL 
 
     useEffect(() => {
         const getMovie = async () => {
-            const url = 'https://tvshow.p.rapidapi.com/Movie/NowPlaying?Page=1&Language=en-US&Adult=true';
+            const url = `${baseUrl}/${group}?Page=${page}&Language=en-US&Adult=true`;
             const options = {
                 method: 'GET',
                 headers: {
-                    'x-rapidapi-key': 'bbb670fbe7msh06e0ff277b81e75p1203c3jsnbdb467292e56',
+                    'x-rapidapi-key': apiKey,
                     'x-rapidapi-host': 'tvshow.p.rapidapi.com'
                 }
             };
@@ -34,12 +39,12 @@ const Home = () => {
         };
 
         getMovie();
-    }, []);
+    }, [group,page]);
 
     return (
       <>
 
-      <Navigation />
+      <Navigation page={page} setPage={setPage} setgroup={setgroup} />
         <div className='flex justify-center items-center' style={{ width: wrapperWidth }} >
             <div className="flex flex-wrap">
                 {movies.length > 0 ? (
--- src/Componenets/Navigation.jsx
+++ src/Componenets/Navigation.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { FaArrowRight,FaArrowLeft } from "react-icons/fa";
 
 
-const Navigation = () => {
+const Navigation = ({setPage,setGroup,page}) => {
   return (
     <div className='relative z-10'>
       <div className='fixed bottom-5 left-5 flex items-center gap-x-2 text-2xl bg-yellow-500/50 rounded-full px-2'>
-        <span className='cursor-pointer'><FaArrowLeft />  </span>
-        <p className='text-xl select-none'>1</p>
-        <span className='cursor-pointer'><FaArrowRight /> </span>
+        <span onClick={()=> page != 1 && setPage(page => page - 1)} className='cursor-pointer'><FaArrowLeft />  </span>
+        <p className='text-xl select-none'>{page}</p>
+        <span onClick={()=> setPage(page => page + 1)} className='cursor-pointer'><FaArrowRight /> </span>
       </div>
       
       <select name='' className='fixed top-5 left-5 bg-gray-200/90 text-sm tracking-wide text-gray-700 uppercase rounded-md outline-none p-1 cursor-pointer hover:bg-gray-200 '>
